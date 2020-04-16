8475cd22b41d43a4a180762e5b68f23a0d06719b
XinRuiiii <1429358374@qq.com>
2020-04-16 17:21:20 +0800

    feature: 前端发送日记功能（纯文本），简单的登录，配置apollo

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import router from './router'
 import App from './App'
 import 'wired-elements'
+import apolloProvider from './apollo'
+
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app');
 
